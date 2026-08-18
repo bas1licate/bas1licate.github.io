4d9b1f50c8622b69ef61577d5c99b2d120ccc120
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-08-18 12:22:06 -0700

    an ordinary boy

--- bytemod/build.js
+++ bytemod/build.js
@@ -1,17 +1,17 @@
-const qi = document // watch this never get used except retroactively due to muscle memory, which defeats the purpose
+const doc = document
 
 const stuff = [];
-qi.head.appendChild(Object.assign(qi.createElement("title"),{innerHTML: "a strange case."}))
-qi.body.append(qi.createElement("br"),"go ahead enter your stuff")
-const fileInput = Object.assign(qi.createElement("input"),{type: "file"}); stuff.push(qi.createElement("br"))
-stuff.push(qi.createElement("label")); stuff.at(-1).append("only files are accepted:",fileInput)
-const bison = Object.assign(qi.createElement("button"),{innerHTML: "run (downloads automatically)"}); stuff.push(bison)
-stuff.push(qi.createElement("ul")); stuff.at(-1).append("operation:");
-["and #","ior #","xor #","negate","negative","plus #", "minus #"].map(x => {let b = Object.assign(qi.createElement("label")); b.append(x,Object.assign(qi.createElement("input"),{type: "radio", name: "op", value: x})); return b}).forEach(x => {l = qi.createElement("li"); l.append(x); stuff.at(-1).append(l)})
-stuff.push(qi.createElement("label")); goat = Object.assign(qi.createElement("input"),{type: "number", min: "0", max: "255"})
+doc.head.appendChild(Object.assign(doc.createElement("title"),{innerHTML: "a strange case."}))
+doc.body.append(doc.createElement("br"),"go ahead enter your stuff")
+const fileInput = Object.assign(doc.createElement("input"),{type: "file"}); stuff.push(doc.createElement("br"))
+stuff.push(doc.createElement("label")); stuff.at(-1).append("only files are accepted:",fileInput)
+const bison = Object.assign(doc.createElement("button"),{innerHTML: "run (downloads automatically)"}); stuff.push(bison)
+stuff.push(doc.createElement("ul")); stuff.at(-1).append("operation:");
+["and #","ior #","xor #","negate","negative","plus #", "minus #"].map(x => {let b = Object.assign(doc.createElement("label")); b.append(x,Object.assign(doc.createElement("input"),{type: "radio", name: "op", value: x})); return b}).forEach(x => {l = doc.createElement("li"); l.append(x); stuff.at(-1).append(l)})
+stuff.push(doc.createElement("label")); goat = Object.assign(doc.createElement("input"),{type: "number", min: "0", max: "255"})
 stuff.at(-1).append("# for binary operationss: ",goat); let q
-qi.querySelectorAll('input[name="op"]').forEach(x => x.addEventListener('click', () => {q = x.value; goat.disabled = ['negate','negative'].includes(q); if (goat.disabled) {goat.value = null}}))
-outl = Object.assign(qi.createElement("a"),{download: ""}); console.log(stuff)
+doc.querySelectorAll('input[name="op"]').forEach(x => x.addEventListener('click', () => {q = x.value; goat.disabled = ['negate','negative'].includes(q); if (goat.disabled) {goat.value = null}}))
+outl = Object.assign(doc.createElement("a"),{download: ""}); console.log(stuff)
 async function basic() {
   const gv = goat.value; const p = new Uint8Array(await fileInput.files[0].arrayBuffer())
   return p.map(x => q=="and #" ? x&gv : q=="ior #" ? x|gv : q=="xor #" ? x^gv : q=="negate" ? ~x : q=="negative" ? -x : q=="plus #" ? x+gv : q=="minus #" ? x-gv : x)
@@ -19,5 +19,5 @@ async function basic() {
 }
 bison.addEventListener('click',() => {basic().then(fd => {outl.href = URL.createObjectURL(new Blob([fd])); outl.click()})})
 
-qi.body.append(...stuff); stuff.length = false
-// qi.getElementbyId("thyself").remove() // only possible use of getElementById in the entire script, which is entirely optional
+doc.body.append(...stuff); stuff.length = false
+// doc.getElementbyId("thyself").remove() // only possible use of getElementById in the entire script, which is entirely optional
