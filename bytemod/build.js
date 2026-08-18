065fc99a99fc49bc542a8d03f05bf56d3b2cefa1
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-08-18 12:02:37 -0700

    good thing it's modular and modulo

--- bytemod/build.js
+++ bytemod/build.js
@@ -7,14 +7,14 @@ const fileInput = Object.assign(qi.createElement("input"),{type: "file"}); stuff
 stuff.push(qi.createElement("label")); stuff.at(-1).append("only files are accepted:",fileInput)
 const bison = Object.assign(qi.createElement("button"),{innerHTML: "run (downloads automatically)"}); stuff.push(bison)
 stuff.push(qi.createElement("ul")); stuff.at(-1).append("operation:");
-["and #","ior #","xor #","negate","negative"].map(x => {let b = Object.assign(qi.createElement("label")); b.append(x,Object.assign(qi.createElement("input"),{type: "radio", name: "op", value: x})); return b}).forEach(x => {l = qi.createElement("li"); l.append(x); stuff.at(-1).append(l)})
+["and #","ior #","xor #","negate","negative","plus #", "minus #"].map(x => {let b = Object.assign(qi.createElement("label")); b.append(x,Object.assign(qi.createElement("input"),{type: "radio", name: "op", value: x})); return b}).forEach(x => {l = qi.createElement("li"); l.append(x); stuff.at(-1).append(l)})
 stuff.push(qi.createElement("label")); goat = Object.assign(qi.createElement("input"),{type: "number", min: "0", max: "255"})
 stuff.at(-1).append("# for binary operationss: ",goat); let q
 qi.querySelectorAll('input[name="op"]').forEach(x => x.addEventListener('click', () => {q = x.value; goat.disabled = ['negate','negative'].includes(q); if (goat.disabled) {goat.value = null}}))
 outl = Object.assign(qi.createElement("a"),{download: ""}); console.log(stuff)
 async function basic() {
   const gv = goat.value; const p = new Uint8Array(await fileInput.files[0].arrayBuffer())
-  /*try {*/return p.map(x => q=="and #" ? x&gv : q=="ior #" ? x|gv : q=="xor #" ? x^gv : q=="negate" ? ~x : q=="negative" ? -x : x)/*}*/
+  return p.map(x => q=="and #" ? x&gv : q=="ior #" ? x|gv : q=="xor #" ? x^gv : q=="negate" ? ~x : q=="negative" ? -x : q=="plus #" ? x+gv : q=="minus #" ? x-gv : x)
   //catch (e) {console.error(e);console.warn(e.message)}
 }
 bison.addEventListener('click',() => {basic().then(fd => {outl.href = URL.createObjectURL(new Blob([fd])); outl.click()})})
