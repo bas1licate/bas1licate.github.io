48249d7ec8a5c501c2d432965dd269c32d1d4392
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-08-18 13:59:46 -0700

    this feels importantish

--- bytemod/build.js
+++ bytemod/build.js
@@ -2,7 +2,7 @@ const doc = document
 
 const stuff = [];
 doc.head.appendChild(Object.assign(doc.createElement("title"),{innerHTML: "a strange case."}))
-doc.body.append(doc.createElement("br"),"go ahead enter your stuff")
+doc.body.append("go ahead enter your stuff",doc.createElement("br"))
 const fileInput = Object.assign(doc.createElement("input"),{type: "file"}); stuff.push(doc.createElement("br"))
 stuff.push(doc.createElement("label")); stuff.at(-1).append("only files are accepted:",fileInput)
 const bison = Object.assign(doc.createElement("button"),{innerHTML: "run (downloads automatically)"}); stuff.push(bison)
@@ -11,10 +11,10 @@ stuff.push(doc.createElement("ul")); stuff.at(-1).append("operation:");
 stuff.push(doc.createElement("label")); goat = Object.assign(doc.createElement("input"),{type: "number", min: "0", max: "255"})
 stuff.at(-1).append("# for binary operationss: ",goat); let q
 doc.querySelectorAll('input[name="op"]').forEach(x => x.addEventListener('click', () => {q = x.value; goat.disabled = ['negate','negative'].includes(q); if (goat.disabled) {goat.value = null}}))
-outl = Object.assign(doc.createElement("a"),{download: ""}); console.log(stuff,q)
+outl = Object.assign(doc.createElement("a"),{download: ""}); console.log(stuff)
 async function basic() {
-  const gv = goat.value; const p = new Uint8Array(await fileInput.files[0].arrayBuffer())
-  return p.map(x => q=="and #" ? x&gv : q=="ior #" ? x|gv : q=="xor #" ? x^gv : q=="negate" ? ~x : q=="negative" ? -x : q=="plus #" ? x+gv : q=="minus #" ? x-gv : x)
+  const gv = goat.value; console.log(q,gv)
+  return new Uint8Array(await fileInput.files[0].arrayBuffer()).map(x => q=="and #" ? x&gv : q=="ior #" ? x|gv : q=="xor #" ? x^gv : q=="negate" ? ~x : q=="negative" ? -x : q=="plus #" ? x+gv : q=="minus #" ? x-gv : x)
   //catch (e) {console.error(e);console.warn(e.message)}
 }
 bison.addEventListener('click',() => {basic().then(fd => {outl.href = URL.createObjectURL(new Blob([fd])); outl.click()})})
