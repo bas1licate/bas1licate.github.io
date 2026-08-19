ddc83a3a3072ee4dd5aafe18ea1c3328e96e38a4
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-08-19 12:56:17 -0700

    fixing a few slightly annoying things

--- bytemod/build.js
+++ bytemod/build.js
@@ -7,11 +7,11 @@ const fileInput = Object.assign(doc.createElement("input"),{type: "file"}); stuf
 stuff.push(doc.createElement("label")); stuff.at(-1).append("only files are accepted:",fileInput)
 const bison = Object.assign(doc.createElement("button"),{innerHTML: "run (downloads automatically)"}); stuff.push(bison)
 stuff.push(doc.createElement("ul")); stuff.at(-1).append("operation:");
-["and #","ior #","xor #","negate","negative","plus #", "minus #"].map(x => {const b = doc.createElement("label"); b.append(x,Object.assign(doc.createElement("input"),{type: "radio", name: "op", value: x})); return b}).forEach(x => {l = doc.createElement("li"); l.append(x); stuff.at(-1).append(l)})
+["and #","ior #","xor #","negate","negative","plus #", "minus #"].map(x => {const b = doc.createElement("label"); b.append(x,Object.assign(doc.createElement("input"),{type: "radio", name: "op", value: x})); return b}).forEach(x => {let l = doc.createElement("li"); l.append(x); stuff.at(-1).append(l)})
 stuff.push(doc.createElement("label")); goat = Object.assign(doc.createElement("input"),{type: "number", min: "0", max: "255"})
 stuff.at(-1).append("# for binary operationss: ",goat); let q
 doc.querySelectorAll('input[name="op"]').forEach(x => x.addEventListener('click', () => {q = x.value; goat.disabled = ['negate','negative'].includes(q); if (goat.disabled) {goat.value = null}}))
-outl = Object.assign(doc.createElement("a"),{download: ""}); console.log(stuff)
+const outl = Object.assign(doc.createElement("a"),{download: ""}); console.log(stuff)
 async function basic() {
   const gv = goat.value; console.log(q,gv)
   return new Uint8Array(await fileInput.files[0].arrayBuffer()).map(x => q=="and #" ? x&gv : q=="ior #" ? x|gv : q=="xor #" ? x^gv : q=="negate" ? ~x : q=="negative" ? -x : q=="plus #" ? x+gv : q=="minus #" ? x-gv : x)
