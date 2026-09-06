31b15887728c6e307bbc026549992ed87e8a220d
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-09-05 23:46:26 -0700

    back to 53d5573 even

--- wavebreak/wb.js
+++ wavebreak/wb.js
@@ -1,13 +1,13 @@
 async function wave(file) {
     const s32 = t => [t%65536,t>>16]
     const to8 = t => new Uint8Array(t.buffer)
-    const FB = new Uint8Array(await file.arrayBuffer()); const sz = 2*Math.ceil(FB.byteLength/2); let x
+    const FB = new Uint8Array(await file.arrayBuffer()); const sz = FB.byteLength; let x
     if (sz > 0xFFFFFF00) {console.error("data too large. maximum size 4,294,967,040 bytes."); x = false} else {x = true}
     if (sz > 0x7FFFFF00) {console.warn("data very large. successful conversion cannot be guaranteed.")}
     const head1 = new Uint16Array([18770,17990,...s32(sz+36),16727,17750])
     const head2 = new Uint16Array([28006,8308,16,0,1,1,44100,0,22664,1,2,16])
     const head3 = new Uint16Array([24932,24948,...s32(sz)])
-    const r = new Uint8Array(x ? sz+44 : 60);
+    const r = new Uint8Array(x ? 2*Math.ceil(sz/2)+44: 60);
     r.set(to8(head1),0);r.set(to8(head2),12);r.set(to8(head3),36)
     r.set(x ? FB : new Uint8Array([91, 111, 98, 106, 101, 99, 116, 32, 80, 114, 111, 109, 105, 115, 101, 93]),44)
     return r;
